19bfbb38170332bd2c134295a452b076214046d7
Tanveer Hossain <stanveer.me@gmail.com>
2024-12-18 01:25:58 +0600

    remove dark mode and add source data

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
 import WallThickness from "./components/WallThickness";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.page}>
       <WallThickness />
-    </>
+      <section className={styles.info}>
+        Find pipeline outer diameter and wall thickness in mm based on the
+        nominal pipe size input according to ASME B36.10 / B36.19. Click
+        <a
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.vanleeuwen.com/en/assets/File/Pipe%20schedule%20overzicht%20website.pdf"
+        >
+          here
+        </a>
+        to get the source data.
+      </section>
+    </div>
   );
 }
